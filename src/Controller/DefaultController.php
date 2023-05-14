54626cc37ae3c3b865b8521209bb6cc15c15a1a4
Aziizi10 <b00130228@mytudublin.ie>
2023-05-14 19:39:43 +0100

    i have added about-us page and added it routes
    also i have added AboutUs page route into DefaultController class.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\WebWorkerController;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
+        $worker_controller = new WebWorkerController("demo_workers.js");
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'worker_controller' => $worker_controller,
         ]);
     }
 
@@ -21,7 +24,8 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/contact_us.html.twig');
     }
-    #[Route('/About-us', name: 'app_About-us')]
+
+    #[Route('/about-us', name: 'app_about_us')]
     public function aboutUs(): Response
     {
         return $this->render('default/about_us.html.twig');
@@ -33,12 +37,13 @@ class DefaultController extends AbstractController
         return $this->render('default/privacy_policy.html.twig');
     }
 
-    #[Route('/location', name: 'app_default')]
+    #[Route('/location', name: 'app_location')]
     public function find(): Response
     {
         return $this->render('location/loaction.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+
     }
 
 }
